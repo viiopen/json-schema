a52670ab69d817813e4644cf19b68b545d6741ff
Benjamin Matics <bmatics@viimed.com>
2016-08-01 15:48:13 -0400

    fixing uri retriever so it actually functions

--- src/JsonSchema/Uri/UriRetriever.php
+++ src/JsonSchema/Uri/UriRetriever.php
@@ -94,9 +94,6 @@ class UriRetriever
      */
     public function resolvePointer($jsonSchema, $uri)
     {
-        // viiopen - until our system actually resolves pointers on the server, just...
-        return json_decode('{}');
-
         $resolver = new UriResolver();
         $parsed = $resolver->parse($uri);
         if (empty($parsed['fragment'])) {
